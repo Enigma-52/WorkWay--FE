c2607d245d849f49d0f16911fedfe728c07faff7
Rohit Singh <rohit.singh.33521@gmail.com>
2026-01-10 04:57:00 +0530

    Add GA

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,23 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-PMBBRGCPM5"
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PMBBRGCPM5');
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
